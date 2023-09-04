aa5d7f4f53f39d8c0905eeeb719eefc6d6ea2b69
l3andro32777 <200221041@estudantes.ips.pt>
2023-09-04 18:39:35 +0100

    melhorias

--- lib/testimonials.ts
+++ lib/testimonials.ts
@@ -25,4 +25,11 @@ export const testimonials = [
       content: "Agile, outside the box and solution-oriented. Leandro’s cocktail of skills make him a real must-have in any team.",
       image: "https://media.licdn.com/dms/image/C4E03AQFQuSNkTRA5kQ/profile-displayphoto-shrink_400_400/0/1654092432333?e=1698883200&v=beta&t=tAD5qe5aMJcPUFHj7QJaA3_6a5ooGzY2qH2pS6AYRKk"
     },
+    {
+      name: 'Raquel Santos',
+      role: 'Enabling Startups @ Build Up Labs & Startup Grind Lisbon',
+      date: '04/09/2023',
+      content: "Always open to new challenges, Leandro listens actively, thinks critically and delivers amazing designs! It's surely a pleasure to work with Leandro.",
+      image: "https://media.licdn.com/dms/image/C4E03AQHew0fANAxv7A/profile-displayphoto-shrink_400_400/0/1621267161335?e=1699488000&v=beta&t=-1L2_sSUnCyiGUeC8R1LcV0-cf89hkH4Rx0O5wtk3XU"
+    },
   ];
