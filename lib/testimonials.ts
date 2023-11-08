4531f64e8b88ce85cbd5b390c489f5b2a860ac02
l3andro32777 <200221041@estudantes.ips.pt>
2023-11-08 18:11:01 +0000

    melhorias

--- lib/testimonials.ts
+++ lib/testimonials.ts
@@ -16,14 +16,14 @@ export const testimonials = [
       role: 'CEO @Tap My Back / EiR @Build Up Labs',
       date: '24/08/2023',
       content: "Consistently impressive! Leandro, elevates collaboration through clear mockup presentations, fostering seamless teamwork. Redefines design excellence, enhancing user experiences across projects. A valuable asset to any team.",
-      image: "https://media.licdn.com/dms/image/D5603AQFucZUS6OILnw/profile-displayphoto-shrink_400_400/0/1692466229402?e=1698278400&v=beta&t=kcsMY35QabAt89y_EhTT0uzhP-X6MKp-jWVrwtUWyGc"
+      image: "https://media.licdn.com/dms/image/D5603AQFucZUS6OILnw/profile-displayphoto-shrink_800_800/0/1692466229786?e=1704931200&v=beta&t=bU16FIPzQmZn4pLbtGvIpn_8erbHjM5l2uGhoTkIKs4"
     },
     {
       name: 'Carlos Geraldes Martins',
       role: 'Strategy & Investment 🚀',
       date: '29/08/2023',
       content: "Agile, outside the box and solution-oriented. Leandro’s cocktail of skills make him a real must-have in any team.",
-      image: "https://media.licdn.com/dms/image/C4E03AQFQuSNkTRA5kQ/profile-displayphoto-shrink_400_400/0/1654092432333?e=1698883200&v=beta&t=tAD5qe5aMJcPUFHj7QJaA3_6a5ooGzY2qH2pS6AYRKk"
+      image: "https://media.licdn.com/dms/image/C4E03AQFQuSNkTRA5kQ/profile-displayphoto-shrink_800_800/0/1654092432846?e=1704931200&v=beta&t=dgaP6apQ_4ZfbmShta_aG6K722S0Q-ZqYK_umtdMCeU"
     },
     {
       name: 'Raquel Santos',
@@ -31,5 +31,12 @@ export const testimonials = [
       date: '04/09/2023',
       content: "Always open to new challenges, Leandro listens actively, thinks critically and delivers amazing designs! It's surely a pleasure to work with Leandro.",
       image: "https://media.licdn.com/dms/image/C4E03AQHew0fANAxv7A/profile-displayphoto-shrink_400_400/0/1621267161335?e=1699488000&v=beta&t=-1L2_sSUnCyiGUeC8R1LcV0-cf89hkH4Rx0O5wtk3XU"
+    },
+    {
+      name: 'João Pereira',
+      role: 'CTO/EiR @ Build Up Labs',
+      date: '08/11/2023',
+      content: "Leandro is a genuine Creative Technologist, seamlessly blending his design skills with his technical knowledge to craft outstanding user experiences. Every challenge he encountered was an opportunity to see him learn and grow. He embodies the perfect fusion of creativity and technology.",
+      image: "https://media.licdn.com/dms/image/D4D03AQE1hbULOM-a3g/profile-displayphoto-shrink_800_800/0/1690810564336?e=1704931200&v=beta&t=w8DGIsrGlCe6-0eovD8G06LIM4FCwCt8S4ohRjFWle4"
     },
   ];
